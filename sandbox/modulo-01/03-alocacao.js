80724883f5ba45a79e767ef7824866ab9f8dd1c3
joao.assuncao <jrego@id.uff.br>
2026-09-12 12:21:57 -0300

    feat: Exercise - Added test cases.

--- sandbox/modulo-01/03-alocacao.js
+++ sandbox/modulo-01/03-alocacao.js
@@ -1,9 +1,9 @@
 function dividirEmParcelas(totalCentavos, numeroParcelas) {
-  if (!(Number.isInteger(numeroParcelas) && numeroParcelas > 1)) {
+  if (!(Number.isInteger(numeroParcelas) && numeroParcelas >= 1)) {
     throw new Error("O número de parcelas deve ser inteiro e maior que zero");
   }
 
-  if (!(Number.isInteger(totalCentavos) && totalCentavos > 0)) {
+  if (!(Number.isInteger(totalCentavos) && totalCentavos >= 0)) {
     throw new Error("O número de centavos deve ser inteiro e maior que zero");
   }
 
@@ -44,7 +44,7 @@ function somar(arrayParcelas) {
 function provar(totalCentavos, numeroParcelas) {
   let arrayParcelas = dividirEmParcelas(totalCentavos, numeroParcelas);
 
-  if (soma(arrayParcelas) === totalCentavos) {
+  if (somar(arrayParcelas) === totalCentavos) {
     return true;
   }
 
@@ -54,4 +54,13 @@ function provar(totalCentavos, numeroParcelas) {
 console.log(provar(1750000, 6));
 console.log(provar(1200000, 6));
 console.log(provar(100, 3));
-console.log(provar());
+console.log(provar(1, 1));
+console.log(provar(10, 10));
+
+console.log(dividirEmParcelas(5, 10));
+console.log(dividirEmParcelas(0, 0));
+
+/**
+ 1 - Essa politica é razoável, pois o sistema não paga nada e no final, a soma dos pagamento é igual ao total.
+ 2 - Não consigo pensar em outra razoável.
+ */
