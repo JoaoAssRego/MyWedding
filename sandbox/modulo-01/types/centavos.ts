090f3c36b9c415a45879673176cd9b434909fd36
JoaoAssRego <jrego@id.uff.br>
2026-09-23 19:19:54 -0300

    fix: verification inside the if was wrong

--- sandbox/modulo-01/types/centavos.ts
+++ sandbox/modulo-01/types/centavos.ts
@@ -1,7 +1,7 @@
 export type Centavos = number & { readonly __brand: "Centavos" };
 
 export function Centavos(valor: number): Centavos {
-    if (valor < 0 && Number.isInteger(valor)) throw new Error("Valor deve ser maior que 0");
+    if (valor < 0 && !Number.isInteger(valor)) throw new Error("Valor deve ser maior que 0");
 
     return valor as Centavos;
 }
