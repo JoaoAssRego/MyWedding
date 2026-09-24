730a13b1b38097cbed5fa48f8c14c09c3a460173
JoaoAssRego <jrego@id.uff.br>
2026-09-24 18:10:32 -0300

    fix(centavos.ts): if logic on l4 was wrong

--- sandbox/modulo-01/types/centavos.ts
+++ sandbox/modulo-01/types/centavos.ts
@@ -1,7 +1,7 @@
 export type Centavos = number & { readonly __brand: "Centavos" };
 
 export function Centavos(valor: number): Centavos {
-    if (valor < 0 && !Number.isInteger(valor)) throw new Error("Valor deve ser maior que 0 e Inteiro");
+    if (valor < 0 || !Number.isInteger(valor)) throw new Error("Valor deve ser maior que 0 e Inteiro");
 
     return valor as Centavos;
 }
